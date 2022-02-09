a7a20848c6887a9de197546abc641492d3e9fad4
Sahar Halim <saharhaim151@gmail.com>
2022-02-09 02:37:46 +0000

    add more robust test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
-   # SETUP
-   before :each do
+  # SETUP
+  before :each do
     @category = Category.create! name: 'Apparel'
 
     10.times do |n|
@@ -20,11 +20,10 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # ACT
     visit root_path
 
-    # DEBUG / VERIFY
-    save_screenshot
-    
-    # VERIFY
-    expect(page).to have_css 'article.product'
-  end
+    # DEBUG
+    #save_screenshot
 
+    # VERIFY
+    expect(page).to have_css 'article.product', count: 10
+  end
 end
