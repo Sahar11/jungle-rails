884a1919c0f1b66f22668d43d692e0448b1e95b9
Sahar Halim <saharhaim151@gmail.com>
2022-02-09 00:13:51 +0000

    add feature spec: home page

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    save_screenshot
+    
+    # VERIFY
+    expect(page).to have_css 'article.product'
+  end
+
+end
